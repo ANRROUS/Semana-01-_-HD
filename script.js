c8e1dbbe40046adc4e80bd88fd1235f9d3a9b2af
ANRROUS <arthuropipa@gmail.com>
2025-03-29 20:40:07 -0500

    Act. N°03 script.js

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 
 function factorialRecursivo(numero){
+    if(numero < 0){
+        console.log("Número invalido para calcular factorial, ingrese un número mayor o igual a 0");
+    }
     if(numero === 0){
         return 1
     }
